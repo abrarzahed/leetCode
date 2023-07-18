19a5bdd46f51a48c52169622dee99124ba55d26a
abrarzahed <abrarzahed6986@gmail.com>
2023-07-18 12:10:53 +0600

    completed problem 20

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /****************************************** 
 COMMENT: 200. Number of Islands
-// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
-// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+ An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 ******************************************/
 /*
 const numIslands = function (grid) {
@@ -67,4 +67,38 @@ console.log(
     ["0", "0", "0", "0", "0"],
   ])
 );
+*/
+
+/****************************************** 
+COMMENT:    20. Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ An input string is valid if:
+ Open brackets must be closed by the same type of brackets.
+ Open brackets must be closed in the correct order.
+ Every close bracket has a corresponding open bracket of the same type.
+******************************************/
+/*
+const isValid = function (s) {
+  const stack = [];
+  const parens = "() {} []";
+  let i = 0;
+
+  while (i < s.length) {
+    stack.push(s[i]);
+    i++;
+
+    let open = stack[stack.length - 2];
+    let close = stack[stack.length - 1];
+
+    let potParens = open + close;
+
+    if (parens.includes(potParens)) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid("()(]{}"));
 */
